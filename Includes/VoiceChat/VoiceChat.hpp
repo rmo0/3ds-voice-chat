7917bcc730655b8fdc0c62263bc94adb1532354d
rmo0 <fh8pwn9k9x@privaterelay.appleid.com>
2026-02-10 05:34:51 +0900

    add IP func

--- Includes/VoiceChat/VoiceChat.hpp
+++ Includes/VoiceChat/VoiceChat.hpp
@@ -7,6 +7,7 @@
 #include "Network/Socket.hpp"
 #include "types.h"
 #include "Unicode.h"
+#include <string>
 
 namespace CTRPluginFramework
 {
@@ -22,6 +23,9 @@ namespace CTRPluginFramework
         bool IsRunning() const { return _running; }
         void Update();
 
+        void SetServerIP(const std::string& ip) { _server_ip = ip; }
+        const std::string& GetServerIP() const { return _server_ip; }
+
     private:
         bool _running;
         bool _ptt_pressed;
@@ -34,6 +38,7 @@ namespace CTRPluginFramework
         Socket _socket;
         Handle _mic_event;
         u32 _mic_last_offset;
+        std::string _server_ip;
 
         void UpdateMicSend();
         void UpdateRecvPlay();
