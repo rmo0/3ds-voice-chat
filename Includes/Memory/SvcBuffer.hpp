dfff799eeb9810b4588bbbbdd4d91bce8e2ca3bf
rmo0 <fh8pwn9k9x@privaterelay.appleid.com>
2026-02-15 20:49:54 +0900

    Memory buffer definitions

--- Includes/Memory/SvcBuffer.hpp
+++ Includes/Memory/SvcBuffer.hpp
@@ -9,10 +9,12 @@ namespace CTRPluginFramework
     {
         u32 addr;
         u32 size;
+        bool alloc_fixed;
 
-        SvcBuffer() : addr(0), size(0) {}
+        SvcBuffer() : addr(0), size(0), alloc_fixed(false) {}
 
         bool Alloc(u32 size_bytes);
+        bool AllocFixed(u32 fixed_addr, u32 size_bytes);
         void Free();
 
         void* Ptr() const { return addr ? reinterpret_cast<void*>(addr) : nullptr; }
